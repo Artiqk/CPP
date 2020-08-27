d481cb5a75738595f644ca32b941de4880d0c97c
Artiqk <dexkyo98@gmail.com>
2020-08-27 15:30:17 +0200

    Added function to use NSH and optimized code and hash algorithm

--- NSH.cpp
+++ NSH.cpp
@@ -6,67 +6,46 @@
 
 using namespace std;
 
-vector<string> decToBinary(unsigned long long n);
-string getHashString(bitset<63> binData);
+string nsh(string wordToHash){
+	const unsigned long long key = 3197729356749581189;
 
-int main(void)
-{
-	vector<string> binaryNb;
-	bitset<63> data;
+	bitset<64> binData;
+	stringstream hash;
 	int zBitCount = 0;
 	int oBitCount = 0;
-	const unsigned long long primeKey = 3197729356749581189;
-	unsigned long long asciiMsg = 0;
-	string msg;
 
-	cout << "Enter text to hash : "; cin >> msg;
+	unsigned long long calcMsg = 0;
 
-	for(int i = 0; i < msg.length(); i++)
-		asciiMsg += int(msg[i]);
-
-	asciiMsg *= primeKey;
-	binaryNb = decToBinary(asciiMsg);
-
-	for(int i = 0; i < binaryNb.size(); i++){
-		if(binaryNb[i] == "0")
-			data[i] = 0;
-		else if(binaryNb[i] == "1")
-			data[i] = 1;
+	for(int i = 0; i < wordToHash.length(); i++){
+		calcMsg += (int(wordToHash[i]) % 9) * i + (int(wordToHash[i]));
 	}
 
-	for(int i = 0; i < data.size(); i++){
-		if(data[i] == 0)
+	calcMsg *= key;
+	binData = decToBinary(calcMsg);
+
+	for(int i = 0; i < binData.size(); i++){
+		if(binData[i] == 0)
 			zBitCount++;
-		else if(data[i] == 1)
+		else if(binData[i] == 1)
 			oBitCount++;
 	}
 
 	if(zBitCount > oBitCount){
-		for(int i = 0; i < data.size(); i++){
-			if(data[i] == 0)
-				data[i] = 1;
-			else if(data[i] == 1)
-				data[i] = 0;
+		for(int i = 0; i < binData.size(); i++){
+			if(binData[i] == 0)
+				binData[i] = 1;
+			else if(binData[i] == 1)
+				binData[i] = 0;
 		}
 	}
 
-	string hash = getHashString(data);
-
-	cout << "NSH = " << hash << endl;
-
-	return 0;
-}
-
-string getHashString(bitset<63> binData){
-	stringstream hash;
 	hash << hex << binData.to_ullong();
+	
 	return hash.str();
 }
 
-
-vector<string> decToBinary(unsigned long long n) 
+bitset<64> decToBinary(unsigned long long n) 
 {
-	vector<string> bin;
 	int binaryNum[64];
 	int i = 0; 
 
@@ -76,8 +55,10 @@ vector<string> decToBinary(unsigned long long n)
 	    i++; 
 	}
 
-	for (int j = i - 1; j >= 0; j--)
-	    bin.push_back(to_string(binaryNum[j]));
+	bitset<64> binData;
 
-	return bin;
+	for (int j = i - 1; j >= 0; j--)
+	    binData[j] = binaryNum[j];
+
+	return binData;
 }
